f31dccd3ecd7aa1252429710d5210e231f3026ef
Hagarde <43098624+Hagarde@users.noreply.github.com>
2021-08-01 15:25:04 +0200

    ç afait lonnnnngtemps

--- 1.py
+++ 1.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import selenium
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 import time
-import threading
 
+import threading
 def check_play():
     try:  
-        if driver.find_elements_by_css_selector("button[class='control-button spoticon-pause-16 control-button--circled']") == [] :
-            bouton_play = driver.find_element_by_css_selector("button[class='control-button spoticon-play-16 control-button--circled']")
+        bouton_play = driver.find_element_by_xpath("/html/body/div[4]/div/div[2]/div[3]/main/div[2]/div[2]/div/div/div[2]/section/div[2]/div[2]/div/button[1]")
+        bouton_play_mode = bouton_play.get_attribute("title")
+        if (bouton_play_mode == "Lecture"):
             bouton_play.click()
     except Exception:
         pass
 def check_repeat():
     try:
-        if driver.find_elements_by_css_selector("button[class='control-button spoticon-repeat-16']") != [] :
-            etat_no_repeat = driver.find_element_by_css_selector("button[class='control-button spoticon-repeat-16']")
-            etat_no_repeat.click()
+        bouton_repeat = driver.find_element_by_xpath('/html/body/div[4]/div/div[2]/div[2]/footer/div/div[2]/div/div[1]/button[5]')
+        if (bouton_repeat.get_attribute("aria-checked") != "true"):
+            bouton_repeat.click()
     except Exception:
         pass   
 def check_time():
@@ -33,35 +34,59 @@ def refresh():
     threading.Timer(3600.0, refresh).start()
     driver.refresh()
 
+def accept_cookies():
+    if (driver.find_elements_by_xpath('/html/body/div[12]/div[3]/div/div/div[2]/div/button[1]') != []):
+        bouton_cookies = driver.find_element_by_xpath('/html/body/div[12]/div[3]/div/div/div[2]/div/button[1]')
+        bouton_cookies.click()
+
+def service_test():
+    if (driver.find_elements_by_xpath('/html/body/div[3]/div/div[3]/div/div[2]/button') != []):
+        bouton_fermer = driver.find_element_by_xpath('/html/body/div[3]/div/div[3]/div/div[2]/button')
+        bouton_fermer.click()
+
 # 2 phase de navigation
 def navi (email, password):
-    driver.get("https://open.spotify.com/playlist/7MQ2ATqqdDTnlx3UUDHe5d?si=d3bJe6odSe-b2Ww8obRR_A")
-    time.sleep(3)
+    # veuillez renseignez votre playlist spotify dans les guillemets
+    # exemple : https://open.spotify.com/playlist/5UsnYdUjn9I9uDK4ZzcUZV
+    driver.get("votre playlist spotify")
+    time.sleep(1)
 
-    bouton_login = driver.find_element_by_xpath('//*[@id="main"]/div/div[2]/div[1]/header/div[5]/button[2]')
+    accept_cookies()
+    time.sleep(1)
+    service_test()
+    time.sleep(1)
+
+    bouton_login = driver.find_element_by_xpath('/html/body/div[3]/div/div[2]/div[1]/header/div[5]/button[2]')
     bouton_login.click()
-    time.sleep(6)
+    time.sleep(3)
     champ_email = driver.find_element(By.ID, "login-username")
     champ_email.send_keys(email + Keys.TAB)
     champ_password = driver.find_element(By.ID, "login-password")
     champ_password.send_keys(password + Keys.ENTER)
 
     time.sleep(5)
-
-    bouton_play = driver.find_element_by_xpath('/html/body/div[4]/div/div[2]/div[4]/main/div/div[2]/div/div/div[2]/section/div[3]/div/button[1]')
-    bouton_play.click()
-
-
-    # mettre la boucle on click jsuque'à ce qu'on soit en mode repeat simple
-    time.sleep(2)
-        
+    
     check_play()
     check_repeat()
 
     check_time()
     refresh()
+
+    time.sleep(2)
+
+    bouton_play = driver.find_element_by_xpath('/html/body/div[4]/div/div[2]/div[2]/footer/div/div[2]/div/div[1]/button[3]')
+    bouton_play.click()
     
 options = Options()
 options.add_experimental_option('useAutomationExtension', False)
-driver = webdriver.Chrome('/Users/aveyronvictor/Documents/AUTO/chromedriver', options=options)
-navi("kskdfjj@gmail.com", "azerty123")
+# veuillez renseigner votre chemin vers votre chromedriver dans les guillemets
+# exemple : '/Users/aveyronvictor/Documents/GINFO/Automatisation-/chromedriver-1'
+driver = webdriver.Chrome('/Users/aveyronvictor/Documents/GINFO/Automatisation-/chromedriver-1', options=options)
+
+# où comme cela:
+# driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+# cependant vous devez importer ceci:
+# from webdriver_manager.chrome import ChromeDriverManager
+
+# veuillez renseigner votre adresse mail et le mot de passe de votre compte spotify dans les guillemets
+navi("obt64512@zwoho.com", "azerty123")
